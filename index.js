79f6958bdd03cc90b203cc939a075cc498d9a1c2
donellgill <donell.gill@outlook.com>
2021-04-12 21:56:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+    let friends = ["Lisa", "Kaitlin", "Jan"];
+    let event = "surprise"; 
+
+function writeCards(friends, event){
+     
+    let birthday = [];
+    for (let i = 0; i < friends.length; i++){
+    
+        friends.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`);
+        
+    }
+    return birthday;
+}
+
+writeCards();
+
+ let num = 10;
+
+function countDown(num){
+   
+    while (num >= 0)
+    console.log(num--);
+
+    
+
+}
+
+countDown();
